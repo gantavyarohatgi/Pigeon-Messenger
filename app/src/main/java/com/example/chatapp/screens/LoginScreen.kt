98c4dc6f502c977a4b84d8df5ba7ef002f4c85db
DhruvRastogi-123 <powerfulrastogi@gmail.com>
2024-03-14 22:01:13 +0530

    Created The Login Page

--- app/src/main/java/com/example/chatapp/screens/LoginScreen.kt
+++ app/src/main/java/com/example/chatapp/screens/LoginScreen.kt
@@ -5,52 +5,146 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.chatapp.components.CButton
+import com.example.chatapp.components.CTextField
+import com.example.chatapp.ui.theme.AlegreyaFontFamily
+import com.example.chatapp.ui.theme.AlegreyaSansFontFamily
 import com.example.chatapp.R
-import com.example.chatapp.components.ButtonComponent
-import com.example.chatapp.components.EmailTextFieldComponent
-import com.example.chatapp.components.PasswordTextFieldComponent
-import com.example.chatapp.components.TextComponent
 
 @Preview
 @Composable
-fun LoginScreen() {
-    Card(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.White)
+fun LoginScreen(
+//    navController: NavHostController
+) {
+    Surface(
+        color = Color(0xFF253334),
+        modifier = Modifier.fillMaxSize()
     ) {
+        Image(painter = painterResource(id = R.drawable.loginbk),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(190.dp),
+            contentScale = ContentScale.Crop
+
+        )
+
+
+        Box(modifier =  Modifier.fillMaxSize()){
+            /// Background Image
+
+
+            /// Content
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 24.dp, vertical = 48.dp)
+            ) {
+
+                // Logo
+                Image(painter = painterResource(id = R.drawable.logotr),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(top = 54.dp)
+                        .height(100.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+
+                Text(text = "Sign In",
+                    style = TextStyle(
+                        fontSize = 28.sp,
+                        fontFamily = AlegreyaFontFamily,
+                        fontWeight = FontWeight(500),
+                        color = Color.White
+                    ),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Text("",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontFamily = AlegreyaSansFontFamily,
+                        color = Color(0xB2FFFFFF)
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(bottom = 24.dp)
+                )
+
+                CTextField(hint = "Email Address", value = "" )
+
+                CTextField(hint = "Password", value = "" )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                CButton(text = "Login")
+
+                Row(
+                    modifier = Modifier.padding(top=12.dp, bottom = 52.dp)
+                ){
+                    Text("Don't have an account? ",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = AlegreyaSansFontFamily,
+                            color = Color.White
+                        )
+                    )
+
+                    Text("Register",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = AlegreyaSansFontFamily,
+                            fontWeight = FontWeight(800),
+                            color = Color.White
+                        ),
+                        modifier = Modifier.clickable {
+//                            navController.navigate("login")
+                        }
+                    )
+
+
+                }
+
+            }
+
+        }
 
     }
 }
 
-
-@Preview
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
 @Composable
-fun DefaultPreviewOfLoginScreen() {
+fun SignInScreenPreview() {
     LoginScreen()
 }
+
