594df8ea4cdf14671888a7cf4867b280dad63133
fourhaan <farhaan2k4@gmail.com>
2024-03-16 01:34:00 +0530

    pulled rastogi

--- app/src/main/java/com/example/chatapp/utils/FirebaseUtils.kt
+++ app/src/main/java/com/example/chatapp/utils/FirebaseUtils.kt
@@ -5,7 +5,7 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class FirebaseUtils {
+class FirebaseUtils{
     val auth: FirebaseAuth = FirebaseAuth.getInstance()
     val database: DatabaseReference = Firebase.database.reference
 }
